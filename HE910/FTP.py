95e2ba63269e5a8ae7300ebdafde666a5e2441ed
sherpei <sharpei.pit@yandex.ru>
2017-01-04 22:15:11 +0400

    Update FTP.py

--- HE910/FTP.py
+++ HE910/FTP.py
@@ -1,37 +1,3 @@
-# -*- coding: iso-8859-1 -*-
-#Telit Extensions
-#
-#Copyright © 2009, Telit Communications S.p.A.
-#All rights reserved.
-#
-#Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-#
-#Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-#
-#Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in
-#the documentation and/or other materials provided with the distribution.
-#
-#
-#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS``AS IS'' AND ANY EXPRESS OR IMPLIED
-#WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-#PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
-#DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-#PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-#HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-#NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-#POSSIBILITY OF SUCH DAMAGE.
-#
-
-"""
-FTP.py
-Feb 2012|corrected and tested for Python 2.7.2 on HE910
-- added coding line on top
-- changed MOD with time
-- changed MDM.receive with MDM.read (see MDM_receive function)
-Jan 2009|copyright added, checked & tested
-06/11/06|Sgonico|fb&lt|1st ver
-Demonstrates FTP operations.
-"""
 import time
 import SER
 import MDM
@@ -60,7 +26,7 @@ def MDM_receive(timeout):
     return res
 
 
-print 'FTP.py\r\n© 2009 Telit Communications\r'
+print 'FTP.py\r\nÂ© 2009 Telit Communications\r'
 
 print 'SIM PIN  %s\r\n' % (SIMPIN)
 res = MDM.send('AT+CPIN=' + SIMPIN + '"\r', 2)
